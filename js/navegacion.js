e7b44d407e2c689dc8c8f642b8e0c5103657396c
TomatesConManjar <dmanque3@gmail.com>
2026-04-11 23:00:05 -0400

    Update navegacion.js

--- js/navegacion.js
+++ js/navegacion.js
@@ -41,7 +41,7 @@ function cargarSeccion(seccion) {
                 if (seccion === 'estadisticas' && typeof window.renderTeamStats === 'function') {
                     window.renderTeamStats();
                 }
-            }, 100);
+            }, 200);
             
             // Cerrar menú móvil si está abierto
             const mobileMenu = document.getElementById('mobile-menu');

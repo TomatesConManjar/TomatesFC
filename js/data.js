3b80169c8d3169678e158fdc145548ac645345e1
TomatesConManjar <dmanque3@gmail.com>
2026-09-06 01:58:10 -0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -884,7 +884,6 @@ const jugadoresData = {
     },
     'francisco-lizama': {
         nombre: 'Francisco Lizama',
-        numero: 6,
         posicion: 'Defensa',
         frase: 'La última línea de defensa, siempre alerta',
         imagenCamiseta: 'images/dorsal_lizama6.jpg',
